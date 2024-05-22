5450cd4e0afafba5370d6cfd63b9eda5b0213cd2
sinistri <balestri@gmail.com>
2024-05-22 11:21:54 -0300

    "cambio"

--- tickets.js
+++
@@ -1,8 +0,0 @@
-
-// Carga los tickets almacenados en el array
-const listaTickets = document.getElementById('listaTickets');
-tickets.forEach((ticket) => {
-    const li = document.createElement('li');
-    li.textContent = `${ticket.nombre}: ${ticket.descripcion} (${ticket.fecha})`;
-    listaTickets.appendChild(li); // Agrega el elemento de lista al DOM
-});
